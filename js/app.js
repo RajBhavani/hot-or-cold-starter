f940c3ff3e557294bf8325d7e1de85bc8966d3f7
RajBhavani <rbhavani@outlook.com>
2016-09-01 13:53:44 -0400

    Updated app.js

--- js/app.js
+++ js/app.js
@@ -33,45 +33,45 @@ var game =function(){
 		 			feedbackObject.text("you guessed it ");
 		 			guessEntry(entry);
 		 		} else if ((computerNumber - 5) < entry && entry < (computerNumber + 5)) { /*HOT*/
-                feedbackObject.text("You are HOT!");
-                guessEntry(entry);
-            } else if ((computerNumber - 10) < entry && entry < (computerNumber + 10)) { /*WARM*/
-                feedbackObject.text("Getting WARM!");
-                guessEntry(entry);
-            } else if ((computerNumber - 17) < entry && entry < (computerNumber + 17)) { /*COOL*/
-                feedbackObject.text("Aren't you COOL!");
-                guessEntry(entry);
-            } else if ((computerNumber - 30) < entry && entry < (computerNumber + 30)) { /*COLD*/
-                feedbackObject.text("Uh oh! You're COLD as ice!");
-                guessEntry(entry);
-            } else { /*Too far*/
-                feedbackObject.text("Bad news! You couldn't be farther!");
-                guessEntry(entry);
-            }
+                			feedbackObject.text("You are HOT!");
+					 guessEntry(entry);
+            			} else if ((computerNumber - 10) < entry && entry < (computerNumber + 10)) { /*WARM*/
+                			feedbackObject.text("Getting WARM!");
+                			guessEntry(entry);
+            			} else if ((computerNumber - 17) < entry && entry < (computerNumber + 17)) { /*COOL*/
+                			feedbackObject.text("Aren't you COOL!");
+        				 guessEntry(entry);
+    				} else if ((computerNumber - 30) < entry && entry < (computerNumber + 30)) { /*COLD*/
+        				 feedbackObject.text("Uh oh! You're COLD as ice!");
+        				guessEntry(entry);
+            			} else { /*Too far*/
+        				 feedbackObject.text("Bad news! You couldn't be farther!");
+                				guessEntry(entry);
+            }			
         };
 
         /*-------- Step 3 --------*/
-        if (1 <= entry && entry <= 100) {
-            $('.game').on('click', '.button', function () {
-                feedback(computerNumber, entry);
-            });
-            $('#userGuess').on('keypress', function (e) {
-                if (e.key === 82) {
-                    feedback(computerNumber, entry);
-                }
-            });
-        } else if (0 > entry || entry > 250) {
-            feedbackObject.text("Very clever! Try a number between 1-100");
-        } else {
-            feedbackObject.text("Oops! Try a number between 1-100");
-        }
+        			if (1 <= entry && entry <= 100) {
+        			 $('.game').on('click', '.button', function () {
+                			feedback(computerNumber, entry);
+            		});
+        			 $('#userGuess').on('keypress', function (e) {
+                		if (e.key === 82) {
+                    		feedback(computerNumber, entry);
+        		 }
+        		 });
+        		} else if (0 > entry || entry > 250) {
+        			 feedbackObject.text("Very clever! Try a number between 1-100");
+			 } else {
+            			feedbackObject.text("Oops! Try a number between 1-100");
+        		}
 
 
     };
 
     /*----Step Five-----*/
-    $('.new').click(function () {
-        game();
+		 $('.new').click(function () {
+        	game();
     });
 
     };
