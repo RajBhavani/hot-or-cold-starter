4647d4cb2b58c2f9fb831ab8f99d98001257c214
RajBhavani <rbhavani@outlook.com>
2016-09-03 09:19:12 -0400

    Updated app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,110 @@
-
 $(document).ready(function(){
+
+	var guessArray = new Array();
+
+// Generate the random number//
+
+function generate()
+{
+	var trump = Math.floor((Math.random()*100)+1);
+	console.log("Secret Number is" + ":" + trump);
+	Secret = trump;
+	return Secret;
+
+}
+// Call generate function
+generate();
+
+// Read userguess number on guess button click
+$("#guessButton").on("click",function(event)
+{
+	var userGuess = $("#userGuess").val();
+	//alert(userGuess);
+	event.preventDefault();
+
+	checkNum(userGuess);
+
+	//Push to guess list
+	guessArray.push(userGuess);
+
+	var counter = $("#guessList li").length;
+	$("#count").text (counter);
+	$("#userGuess").val(""); // Empty User Guess Field
+ 
+});
+
+
+//Compare the userguess with random number//
+function checkNum(userGuess) {
+
+	var diff = Secret - userGuess;
+	diff = Math.abs(diff);
+	console.log(diff);
+	if(isNaN(userGuess))
+	{
+		$("#feedback").text("please enter a valid number between 1 and 100");
+
+	}
+	//Last Guess
+	if ($.inArray(userGuess, guessArray) < 0) 
+		{
+			// Verify valid user entry
+			if (userGuess > 0 && userGuess <= 100 && userGuess % 1 === 0 &&  !isNaN(userGuess))
+
+			{
+				$("#guessList").prepend("<li>" + userGuess); //Guess list is ready
+				
+				if (diff === 0)
+				{
+					$("#feedback").text("You won the game!");
+				}
+				else if (diff < 10)
+				{
+					$("#feedback").text("Hot");
+
+				}
+				else if (diff < 25)
+				{
+					$("#feedback").text("lukewarm");
+				}
+				else if (diff < 50)
+				{
+					$("#feedback").text("Cold");
+				}
+				else if (diff < 75)
+				{
+					$("#feedback").text("Freezing");
+				}
+
+				}	
+			else 
+			{
+				$("#feedback").text("Please try between 1 and 100");
+			}
+        }
+	else
+	{
+		$("#feedback").text("You guessed this number already. Please try another number!");
+	}        
+    
 	
-	/*--- Display information modal box ---*/
+
+	
+}
+
+
+// New Game//
+$('.new').click(function(){
+	generate();
+	guessArray = new Array();
+	$("#count").text(0);
+	$('#guessList li').remove();
+	$('#feedback').text("Make your Guess");
+
+});
+
+
+/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
 
@@ -12,74 +115,7 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+
+
+
 });
-var game =function(){
-		var computerNumber = Math.float((Math.random() *100)+1);//step 1
-
-		 var userGuess = function(){//step 2
-		 	var entry = +$('input[type= "text"]').val();//Entry Variabledeclared
-		 	/*----Feedback--*/
-		 	var feedback = function(computerNumber,entry){
-		 		var guessEntry=0;
-		 		guessObject = $('#count'),
-		 		guessList = $('guessList');
-		 		guessNumber++;
-		 		guessObject.text(guessNumber);
-		 		guessList.append('<li>' + entry +'</li>');
-		 		$ ('input[type="text"]')val("";
-		 			//clear text)
-		 		};
-		 		if (computerNumber == entry){
-		 			feedbackObject.text("you guessed it ");
-		 			guessEntry(entry);
-		 		} else if ((computerNumber - 5) < entry && entry < (computerNumber + 5)) { /*HOT*/
-                			feedbackObject.text("You are HOT!");
-					 guessEntry(entry);
-            			} else if ((computerNumber - 10) < entry && entry < (computerNumber + 10)) { /*WARM*/
-                			feedbackObject.text("Getting WARM!");
-                			guessEntry(entry);
-            			} else if ((computerNumber - 17) < entry && entry < (computerNumber + 17)) { /*COOL*/
-                			feedbackObject.text("Aren't you COOL!");
-        				 guessEntry(entry);
-    				} else if ((computerNumber - 30) < entry && entry < (computerNumber + 30)) { /*COLD*/
-        				 feedbackObject.text("Uh oh! You're COLD as ice!");
-        				guessEntry(entry);
-            			} else { /*Too far*/
-        				 feedbackObject.text("Bad news! You couldn't be farther!");
-                				guessEntry(entry);
-            }			
-        };
-
-        /*-------- Step 3 --------*/
-        			if (1 <= entry && entry <= 100) {
-        			 $('.game').on('click', '.button', function () {
-                			feedback(computerNumber, entry);
-            		});
-        			 $('#userGuess').on('keypress', function (e) {
-                		if (e.key === 82) {
-                    		feedback(computerNumber, entry);
-        		 }
-        		 });
-        		} else if (0 > entry || entry > 250) {
-        			 feedbackObject.text("Very clever! Try a number between 1-100");
-			 } else {
-            			feedbackObject.text("Oops! Try a number between 1-100");
-        		}
-
-
-    };
-
-    /*----Step Five-----*/
-		 $('.new').click(function () {
-        	game();
-    });
-
-    };
-		 	}
-
-		 }
-
-	}
-})
-
-
